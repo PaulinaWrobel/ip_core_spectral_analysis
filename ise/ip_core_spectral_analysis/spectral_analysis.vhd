9e5347c37ddeb202624924ff4307b0af9abf3b80
PaulinaWrobel <paulina.wrobel.93@wp.pl>
2017-03-29 00:30:00 +0200

    ISE

---
+++ ise/ip_core_spectral_analysis/spectral_analysis.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity spectral_analysis is
+
+end entity spectral_analysis;
+
+architecture Behavioral of spectral_analysis is
+
+begin
+
+
+end architecture Behavioral;
+
---
+++ ise/ip_core_spectral_analysis/top.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
+
+entity top is
+end top;
+
+architecture behav of top is
+
+begin
+
+
+end behav;
+
